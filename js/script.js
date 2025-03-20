2ebf90095e6f483ec81b8b03d0fde9e5b3c88c99
tizianotambone <tambonetiziano@gmail.com>
2025-03-20 15:49:12 +0100

    [UPD]ex2fixed

--- js/script.js
+++ js/script.js
@@ -23,21 +23,23 @@ else{
 }
 
 // esercizio 2+
-// indico al player di inserire un numero compreso tra 1-6
-
  
-// assegno la variabile human dove
+// assegno la variabile human dove indico al player di inserire un numero compreso tra 1-6
 let human = prompt("inserisci un numero tra uno e 6");
 console.log(human);
+//  indico la variabile pc dove indico al pc di generare un numero casuale compreso tra 1-6
 let pc =  Math.floor(Math.random()*6)+1;
 console.log(pc);
+// se il numero scritto da human è minore di  quello generato dal pc esce lo scripit hai perso
 
 if(human<pc){
     console.log("hai perso");
 }
+// se il numero scritto da human è maggiore di  quello generato dal pc esce lo scripit hai vinto
 else if (human>pc){
     console.log("hai vinto");
 }
+//  altrimenti esce lo script pareggio
 else{
     console.log("pari");
 }
